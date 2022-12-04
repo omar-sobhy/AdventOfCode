3802d80de934a348383df3f47cd72f288d30ec2a
Omar Sobhy <omar.ahmed.sobhy.96@gmail.com>
2022-12-04 12:41:00 +0400

    Add uninteractivePrompt

--- index.ts
+++ index.ts
@@ -1,11 +1,50 @@
 import { readdir, readFile } from "fs/promises";
-import { argv } from "process";
-import prompts, { Choice, PromptObject } from 'prompts';
+import { argv, exit } from "process";
+import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage';
+import prompts, { Choice } from 'prompts';
 import Runner from "./levels/01";
+
+const sections = [
+    {
+        header: 'AoC runner',
+        content: 'A level runner for Advent of Code',
+    },
+    {
+        header: 'Options',
+        optionList: [
+            {
+                name: 'level',
+                typeLabel: '{underline number}',
+                description: 'The level to run.',
+            },
+            {
+                name: 'input',
+                typeLabel: '{underline fileName}',
+                description: 'Which input file to run. Enter \'main\' or a number corresponding to a test case.',
+            },
+        ],
+    },
+];
+
+const usage = commandLineUsage(sections);
+
+function normalise(n: string): string | undefined {
+    const nNumber = Number(n);
+    if (isNaN(nNumber)) {
+        return undefined;
+    }
+
+    if (1 <= nNumber && nNumber <= 9) {
+        return `0${nNumber}`;
+    }
+
+    return n;
+}
 
 async function interactivePrompt() {
     const mainLevelsDirFiles = await readdir('./levels', { encoding: 'utf-8' });
-    
+
     const filteredLevelsDirFiles = mainLevelsDirFiles.filter((fileName) => {
         return /\d\d/.test(fileName);
     });
@@ -18,7 +57,7 @@ async function interactivePrompt() {
             };
         }
     );
-    
+
     const levelResponse = await prompts({
         type: 'select',
         message: 'Pick level',
@@ -28,9 +67,9 @@ async function interactivePrompt() {
 
     const level = levelResponse.level;
 
-    const choicesForInputPrompt: Choice[] = 
+    const choicesForInputPrompt: Choice[] =
         (await readdir(`./levels/${level}/inputs`, {
-            encoding: 'utf-8' 
+            encoding: 'utf-8'
         })).map((fileName) => {
             return {
                 title: `Input ${fileName}`,
@@ -56,18 +95,89 @@ async function interactivePrompt() {
 
     const { default: defaultExport } = await import(`./levels/${level}`);
     const runner = new defaultExport() as Runner;
-    
+
     const result = await runner.run(input);
 
-    console.log(`Result of running level ${level} using input file ${inputFileName}`);
+    console.log(`Result of running level \`${level}\` using input file \`${input}\``);
     console.log('-------------------------');
     console.log(result);
     console.log('-------------------------');
 }
 
+async function uninteractivePrompt() {
+    const optionsDefinitions = [
+        { name: 'level', alias: 'l', type: String },
+        { name: 'input', alias: 'i', type: String },
+        { name: 'help', alias: 'h', type: Boolean },
+    ];
+
+    const options = commandLineArgs(optionsDefinitions);
+
+    const { level, input, help } = options;
+
+    if (help) {
+        console.log(usage);
+        exit();
+    }
+
+    const mainLevelsDirFiles = await readdir('./levels', { encoding: 'utf-8' });
+
+    const filteredLevelsDirFiles = mainLevelsDirFiles.filter((fileName) => {
+        return /\d\d/.test(fileName);
+    });
+
+    const normalisedLevel = normalise(level);
+    if (!normalisedLevel) {
+        console.log(`Invalid value for \`level\` (got \`${level}\`).`);
+        exit();
+    }
+
+    if (!filteredLevelsDirFiles.find(l => l === normalisedLevel)) {
+        console.log(`Invalid value for \`level\` (got \`${level}\`).`);
+        exit();
+    }
+
+
+    const inputs = await readdir(`./levels/${normalisedLevel}/inputs`, {
+        encoding: 'utf-8'
+    });
+    
+    const normalisedInputNumber = normalise(input);
+
+    if (input.toLowerCase() !== 'main') {
+        if (!normalisedInputNumber) {
+            console.log(`Invalid value for \`input\` (got \`${input}\`).`);
+            exit();
+        }
+        
+        if (!inputs.find(i => i === normalisedInputNumber)) {
+            console.log(`Invalid value for \`input\` (got \`${input}\`).`);
+            exit();
+        }
+    }
+
+    // console.log("?", normalisedLevel, normalisedInputNumber);
+
+    const input_ = await readFile(
+        `./levels/${normalisedLevel}/inputs/${normalisedInputNumber}`,
+        {
+            encoding: 'utf-8',
+        }
+    );
+
+    const { default: defaultExport } = await import(`./levels/${normalisedLevel}`);
+    const runner = new defaultExport() as Runner;
+
+    const result = await runner.run(input_);
+
+    console.log(`Result of running level \`${normalisedLevel}\` using input file \`${normalisedInputNumber}\``);
+    console.log('-------------------------');
+    console.log(result);
+    console.log('-------------------------');
+}
 
 if (argv.length < 3) {
     interactivePrompt();
 } else {
-
+    uninteractivePrompt();
 }
